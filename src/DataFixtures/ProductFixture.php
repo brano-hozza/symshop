8f3046eed318e36bf6baaf13c10cd51de4434d75
Glatoo <darkisharki@gmail.com>
2020-01-13 19:41:59 +0100

    Fix js, favicon, description length

--- src/DataFixtures/ProductFixture.php
+++ src/DataFixtures/ProductFixture.php
@@ -15,7 +15,7 @@ class ProductFixture extends BaseFixture
             $product->setName($this->faker->lastName);
             $product->setDescription($this->faker->text);
             $product->setSize($this->faker->randomFloat(1,30,50));
-            $product->setBrand($this->faker->name);
+            $product->setBrand($this->faker->randomElement(["Adidas", "Converse", "Puma", "Jordan", "Nike", "Reebok", "Vans", "Calvin Klein", "Estos", "Balenciaga", "Gucci", "Givenchy", "Goyard"]));
             $product->setPcs($this->faker->numberBetween(10,30));
             $product->setType($this->faker->randomElement(["Shoes", "T-Shirt", "Headgear", "Trousers"]));
             $product->setPrice($this->faker->randomFloat(2,30,300));
