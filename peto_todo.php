45933e43c471e539a819407230cf03e419cc854e
roulzin <janos.peter.tasnv@gmail.com>
2020-01-27 21:58:53 +0100

    Dokončeny profil, upravene products na telefon, opravene chyby, blog

--- peto_todo.php
+++ peto_todo.php
@@ -3,6 +3,4 @@
  * @todo Nastylovat filter pri produktoch ktory som ti tam dorobil
  * @todo Treba v produktoch dole nastylovat tlacidla na paging
  * @todo Co spravime s tymi tromi bodkami v indexe naboku? Daco tam dajme nejake 3 related itemy ti tam poslem v premennej products
- * @todo Nastylovat blog jednotlivy ako bude vyzerat
- * @todo Zmenit profil usera pridat moznost na reset hesla plus sledovanie objednavok
  */
